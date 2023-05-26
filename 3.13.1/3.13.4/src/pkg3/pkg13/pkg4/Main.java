7b58f729d20b18f4e58d694fb35d298a26e49935
JOHANcalderonperdomo <jcalderonp2008@gmail.com>
2023-05-26 13:00:04 -0500

    subiendo

---
+++ 3.13.1/3.13.4/src/pkg3/pkg13/pkg4/Main.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg3.pkg13.pkg4;
+import java.util.Scanner;
+/**
+ *
+ * @author Ambiente 209-3
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         int num;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("ingrese la cantidad de numeros que desea");
+        num=scan.nextInt();
+    int[] numeros = new int[num]; 
+
+        /*for (int i = 0; i < num; i++) {
+            numeros[i] = i + 1;
+        }
+*/
+
+        for (int i = 0; i < num; i++) {
+            numeros[i] = i + 1;
+            if (numeros[i] % 2!=0){
+               System.out.println("numero impar: "+numeros[i]);
+            }else{
+                System.out.println("numero par: "+numeros[i]);
+            }
+        }
+    }
+    
+}
